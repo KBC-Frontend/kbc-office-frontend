1d6cfa1c4aa4d682f7460ae2f8547c89598cd0f4
KangMins <rkdalsdl98@naver.com>
2024-07-30 14:41:34 +0900

    내 일정 페이지 병합 / 20240730 (#7)
    * 내 일정 페이지
    
    * Update README.md
    
    * 내 일정 페이지 제작
    
    * 내 일정 페이지 제작
    
    ---------
    
    Co-authored-by: KangMins <rkdalsdl98@gmail.com>

---
+++ app/mytask/(component)/(task)/index.tsx
@@ -0,0 +1,48 @@
+import Image from "next/image"
+
+import Spacer from "@/app/(common)/(component)/(spacer)"
+
+import CalendarIcon from "../../../../public/image/calendar.png"
+import FlagIcon from "../../../../public/image/flag.png"
+import styles from "./task.module.css"
+
+export default function Task({
+    onChangeShowPage
+}: TaskProps) {
+    return (
+        <li 
+        className={styles.container}
+        onClick={onChangeShowPage}
+        >
+            <span>일정 이름</span>
+            <div className={styles.info_container}>
+                <div className={styles.data_container}>
+                    <Image
+                    src={CalendarIcon}
+                    alt="달력 아이콘"
+                    />
+                    <Spacer spacing={5} direction="row"/>
+                    <div className={styles.data_wrapper}>
+                        <p>목표 기한</p>
+                        <p>0000년 0월 0일</p>
+                    </div>
+                </div>
+                <div className={styles.data_container}>
+                    <Image
+                    src={FlagIcon}
+                    alt="깃발 아이콘"
+                    />
+                    <Spacer spacing={5} direction="row"/>
+                    <div className={styles.data_wrapper}>
+                        <p>목표 달성까지</p>
+                        <p>0개 남음</p>
+                    </div>
+                </div>
+            </div>
+        </li>
+    )
+}
+
+interface TaskProps {
+    onChangeShowPage: () => void
+}
---
+++ app/mytask/(component)/(top)/main_top.tsx
@@ -0,0 +1,118 @@
+"use client"
+
+import { useState } from "react"
+
+import TextButton from "@/app/(common)/(component)/(button)"
+import Spacer from "@/app/(common)/(component)/(spacer)"
+import TaskFilter from "../(filter)"
+
+import DropdownArrow from "../../../../public/image/arrow_down.png"
+import styles from "./main_top.module.css"
+
+export default function MyTaskMainTop({
+    onOpenRegistWindow,
+}: MyTaskTopProps) {
+    const [openFilterFromDate, setOpenFilterFromDate] = useState<boolean>(false)
+    const [openFilterFromState, setOpenFilterFromState] = useState<boolean>(false)
+    const [fromDateTarget, setFromDateTarget] = useState<FilterFromDate>("시작순")
+    const [fromStateTarget, setFromStateTarget] = useState<FilterFromState>("모두")
+
+    const filter = (target: string | FilterFromDate | FilterFromState) => {
+        switch(target) {
+            case "시작순":
+            case "종료순":
+            case "작업순":
+                setFromDateTarget(target as FilterFromDate)
+                break
+            case "모두":
+            case "진행중":
+            case "중단":
+            case "완료":
+                setFromStateTarget(target as FilterFromState)
+                break
+            default: return
+        }
+    }
+    
+    const fromDate = [
+        {
+            name: "시작순",
+            onClick: filter,
+        },
+        {
+            name: "종료순",
+            onClick: filter,
+        },
+        {
+            name: "작업순",
+            onClick: filter,
+        }
+    ]
+
+    const fromState = [
+        {
+            name: "모두",
+            onClick: filter,
+        },
+        {
+            name: "진행중",
+            onClick: filter,
+        },
+        {
+            name: "중단",
+            onClick: filter,
+        },
+        {
+            name: "완료",
+            onClick: filter,
+        }
+    ]
+
+    return (
+        <div className={styles.container}>
+            <span>내 일정</span>
+            <div className={styles.feature_cotainer}>
+                <div className={styles.filter_container}>
+                    <TextButton 
+                    text={fromStateTarget}
+                    borderRadius={5}
+                    prefixIcon={{ icon: DropdownArrow, rotateOnClick: true }}
+                    onClick={() => setOpenFilterFromState(!openFilterFromState)}
+                    />
+                    <TaskFilter
+                    items={fromState}
+                    isOpen={openFilterFromState}
+                    />
+                </div>
+                <Spacer spacing={10} direction="row"/>
+                <div className={styles.filter_container}>
+                    <TextButton 
+                    text={fromDateTarget}
+                    borderRadius={5}
+                    prefixIcon={{ icon: DropdownArrow, rotateOnClick: true }}
+                    onClick={() => setOpenFilterFromDate(!openFilterFromDate)}
+                    />
+                    <TaskFilter 
+                    isOpen={openFilterFromDate} 
+                    items={fromDate}
+                    />
+                </div>
+                <Spacer spacing={10} direction="row"/>
+                <TextButton 
+                text="일정 만들기"
+                width={100}
+                type="blue"
+                borderRadius={5}
+                onClick={onOpenRegistWindow}
+                />
+            </div>
+        </div>
+    )
+}
+
+interface MyTaskTopProps {
+    onOpenRegistWindow: () => void
+}
+
+type FilterFromDate = "시작순" | "종료순" | "남은 작업 순"
+type FilterFromState = "모두" | "진행중" | "중단" | "완료"
