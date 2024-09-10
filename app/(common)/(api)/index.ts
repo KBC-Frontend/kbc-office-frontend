c090fca81054f7f3152fb681b4bf7420045da3ed
KangMins <rkdalsdl98@naver.com>
2024-09-10 13:57:38 +0900

    edit conflict

--- app/(common)/(api)/index.ts
+++ app/(common)/(api)/index.ts
@@ -164,7 +164,7 @@ interface SuccessResponse<T> extends Response {
     readonly details: string // 결과 상태 상세 메시지
     readonly authorization?: string // JWT 토큰
     readonly data?: T
-    readonly type?: stringd9eed9309bf66966cb3e9c382eb0e3eda48
+    readonly type?: string
 }
 
 interface FailureReponse extends Response {
